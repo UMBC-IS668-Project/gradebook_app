c224d53b75d7e737b3d809964fd08ab4c251661f
danstd <danielst.davis@gmail.com>
2021-04-17 15:43:09 -0400

    Add files via upload

---
+++ gradebook.py
@@ -0,0 +1,93 @@
+from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import ForeignKey
+from flask_migrate import Migrate
+from flask import Flask, redirect, render_template, request, url_for
+from flask_login import login_user, LoginManager, UserMixin, logout_user, login_required, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+from mysql import connector
+
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="",
+    password="",
+    hostname="127.0.0.1",
+    databasename="gradebook",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+app.secret_key = ""
+login_manager = LoginManager()
+login_manager.init_app(app)
+migrate = Migrate(app, db)
+
+class User(UserMixin):
+
+    def __init__(self, username, password_hash):
+        self.username = username
+        self.password_hash = password_hash
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+    def get_id(self):
+        return self.username
+all_users = {
+}
+
+@login_manager.user_loader
+def load_user(user_id):
+    return all_users.get(user_id)
+
+class Comment(db.Model):
+
+    __tablename__ = "comment"
+    commenter = db.Column(db.String(45))
+    commentID = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+    commentTimeStamp = db.Column(db.DateTime, default=datetime.now)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all())
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    comment = Comment(content=request.form["contents"], commenter=current_user.username)
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login_page.html", error=False)
+
+    username = request.form["username"]
+    if username not in all_users:
+        return render_template("login_page.html", error=True)
+    user = all_users[username]
+
+    if not user.check_password(request.form["password"]):
+        return render_template("login_page.html", error=True)
+
+    login_user(user)
+    return redirect(url_for('index'))
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
