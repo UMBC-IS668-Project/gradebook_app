cc533a0b6457b72837c49dd4b5c19dcaee3574e2
danstd <danielst.davis@gmail.com>
2021-04-18 07:09:57 -0400

    changed to snake_case

--- gradebook.py
+++ gradebook.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-#from flask_sqlalchemy import ForeignKey
 from flask_migrate import Migrate
 from flask import Flask, redirect, render_template, request, url_for
 from flask_login import login_user, LoginManager, UserMixin, logout_user, login_required, current_user
@@ -32,37 +31,37 @@ migrate = Migrate(app, db)
 class User(UserMixin, db.Model):
 	
     __tablename__ = 'user'
-    userID = db.Column(db.Integer, primary_key = True)
-    userName = db.Column(db.String(128))
-    passwordHash = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, primary_key = True)
+    user_name = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
 
     def check_password(self, password):
-        return check_password_hash(self.passwordHash, password)
+        return check_password_hash(self.password_hash, password)
 
     def get_id(self):
-        return(self.userName)
+        return(self.user_name)
 
 @login_manager.user_loader
-def load_user(enteredName):
-    return User.query.filter_by(userName = enteredName).first()
+def load_user(entered_name):
+    return User.query.filter_by(user_name = entered_name).first()
 
 class Comment(db.Model):
 
     __tablename__ = "comment"
-    commenterID = db.Column(db.Integer, db.ForeignKey(User.userID))
+    commenter_ID = db.Column(db.Integer, db.ForeignKey(User.user_id))
     commentID = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
-    commentTimeStamp = db.Column(db.DateTime, default=datetime.now)
-    userConstraint = db.relationship('User', foreign_keys=commenterID)
+    comment_time_stamp = db.Column(db.DateTime, default=datetime.now)
+    user_constraint = db.relationship('User', foreign_keys=commenter_ID)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html",commentDisplay=db.session.query(Comment.content, Comment.commenterID, Comment.commentTimeStamp, User.userName).select_from(Comment).join(User).all())
+        return render_template("main_page.html",comment_display=db.session.query(Comment.content, Comment.commenter_ID, Comment.comment_time_stamp, User.user_name).select_from(Comment).join(User).all())
     if not current_user.is_authenticated:
         return redirect(url_for('index'))
 
-    comment = Comment(content=request.form["contents"], commenterID = current_user.userID)
+    comment = Comment(content=request.form["contents"], commenter_ID = current_user.user_id)
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for('index'))
