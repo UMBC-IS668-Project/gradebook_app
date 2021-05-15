6d8b94a6a1380bd784f48f0f7aebacf34af565ba
danstd <danielst.davis@gmail.com>
2021-05-15 16:22:56 -0400

    Added string restriction for form validation

--- gradebook.py
+++ gradebook.py
@@ -7,6 +7,8 @@ from datetime import datetime
 from mysql import connector
 from werkzeug.exceptions import HTTPException
 
+# Breadcrumb set up from https://usersnap.com/blog/breadcrumbs/
+
 app = Flask(__name__)
 app.debug = True
 
@@ -67,7 +69,12 @@ def create():
     # Check if the form has been filled out
     for i in request.form:
         if request.form[i] is None or request.form[i] == "":
-            return render_template("create.html", message="The form must be filled out!")
+            return render_template("create_login.html", message="The form must be filled out!")
+
+    # Check if form entries are within 128 characters
+    for i in request.form:
+        if len(request.form[i]) > 128:
+            return render_template("create_login.html", message="Please enter 128 characters or fewer for each field")
 
     if current_user.is_authenticated:
         return render_template("create_login.html", message="You are already logged in!")
@@ -78,8 +85,6 @@ def create():
 
     if request.form.get("password") != request.form.get("password_confirm"):
         return render_template("create_login.html", message="Your passwords did not match!")
-
-    #Possibly check https://stackoverflow.com/questions/16709638/checking-the-strength-of-a-password-how-to-check-conditions#32542964 for password complexity checking
 
     new_user = User()
     new_user.user_name = request.form["username"]
@@ -103,6 +108,7 @@ def logout():
 @login_required
 def student():
     if request.method == "GET":
+        # Subquery to get assignments using grade entries
         grade_return_inner = db.session.query(
             Assignment.assignment_ID,
             Assignment.assignment_name,
@@ -129,8 +135,6 @@ def student():
                 student_return_inner.c.student_ID == Student.student_ID, isouter=True).join(grade_return_inner,
             grade_return_inner.c.student_ID == Student.student_ID, isouter=True).order_by(Student.first_name)
 
-        assignment_return=Assignment.query.all()
-
         student_outer_return = db.session.query(
            Student.student_ID,
             Student.first_name,
@@ -143,8 +147,7 @@ def student():
         return render_template(
             "students.html",
             student_display=student_outer_return,
-            grade_display=student_return,
-            assignment_display=assignment_return
+            grade_display=student_return
         )
     else:
         return redirect(url_for("index"))
@@ -256,7 +259,12 @@ def create_student():
     # Check if the form has been filled out.
     for i in request.form:
         if request.form[i] is None or request.form[i] == "":
-            return render_template("create_student.html", message="The form must be filled out!")
+            return render_template("create_student.html", message="The form must be filled out")
+
+    # Check if form entries are within 45 characters
+    for i in request.form:
+        if len(request.form[i]) > 45:
+            return render_template("create_student.html", message="Please enter 45 characters or fewer for each field")
 
     # Check if the student has been entered already
     student_check = Student.query.filter_by(
@@ -265,7 +273,7 @@ def create_student():
         email_address=request.form["email_address"].lower()).first()
 
     if student_check is not None:
-        return render_template("create_student.html", message="This entry already exists!")
+        return render_template("create_student.html", message="This entry already exists")
 
     new_student = Student()
     new_student.first_name = request.form["first_name"]
@@ -275,26 +283,32 @@ def create_student():
     db.session.add(new_student)
     db.session.commit()
 
-    return render_template("create_student.html", message="The student was added successfully!")
+    return render_template("create_student.html", message="The student was added successfully")
 
 
-@app.route("/edit_student/<int:edit_ID>", methods=["GET", "PUT", "POST"])
+@app.route("/edit_student/<int:edit_ID>", methods=["GET", "POST"])
 @login_required
 def edit_student(edit_ID=None):
+    student_return = db.session.query(
+        Student.student_ID,
+        Student.first_name,
+        Student.last_name,
+        Student.email_address,
+        Student.major).select_from(Student).filter(Student.student_ID == edit_ID).first()
+
     if request.method == "GET":
         if edit_ID is not None:
-            student_return = db.session.query(
-                Student.student_ID,
-                Student.first_name,
-                Student.last_name,
-                Student.email_address,
-                Student.major).select_from(Student).filter(Student.student_ID == edit_ID).first()
-
             return render_template("edit_student.html", student_display=student_return)
         else:
             return render_template("edit_student.html", student_display="")
 
     # Validate input
+
+    # Check if form entries are within 45 characters
+    for i in request.form:
+        if len(request.form[i]) > 45:
+            return render_template("edit_student.html", student_display=student_return,
+                                   message="Please enter 45 characters or fewer for each field")
 
     # Check if the form has been filled out.
     for i in request.form:
@@ -303,7 +317,7 @@ def edit_student(edit_ID=None):
 
     ed_student = Student.query.filter_by(student_ID=edit_ID).first()
     if ed_student is None:
-        return render_template("edit_student.html", message="Nothing to edit!")
+        return render_template("edit_student.html", message="The student entry could not be found")
 
     ed_student.first_name = request.form["first_name"]
     ed_student.last_name = request.form["last_name"]
@@ -313,7 +327,7 @@ def edit_student(edit_ID=None):
     db.session.commit()
 
     return render_template("edit_student.html", student_display=ed_student,
-                           message="The student was edited successfully!")
+                           message="The student was edited successfully")
 
 
 @app.route("/delete_student/<delete_ID>", methods=["GET", "POST", "DELETE"])
@@ -339,7 +353,7 @@ def delete_student(delete_ID=None):
     db.session.delete(del_student)
     db.session.commit()
 
-    return render_template("delete_student.html", message="The student was deleted successfully!")
+    return render_template("delete_student.html", message="The student was deleted successfully")
 
 
 @app.route("/create_assignment/", methods=["GET", "POST"])
@@ -352,19 +366,25 @@ def create_assignment():
     # Check if anything has been entered.
     for i in request.form:
         if request.form[i] is None or request.form[i] == "":
-            return render_template("create_assignment.html", message="The form must be filled out!")
+            return render_template("create_assignment.html", message="The form must be filled out")
+
+    # Check if form entries are within 45 characters
+    for i in request.form:
+        if len(request.form[i]) > 45:
+            return render_template("create_assignment.html",
+                                   message="Please enter 45 characters or fewer for each field")
 
     # Check if an assignment with a matching name exists
     assignment_check = Assignment.query.filter_by(assignment_name=request.form["assignment_name"]).first()
     if assignment_check is not None:
-        return render_template("create_assignment.html", message="There is already an assignment with this name!")
+        return render_template("create_assignment.html", message="There is already an assignment with this name")
 
     new_assignment = Assignment()
     new_assignment.assignment_name = request.form["assignment_name"]
     db.session.add(new_assignment)
     db.session.commit()
 
-    return render_template("create_assignment.html", message="The assignment was added successfully!")
+    return render_template("create_assignment.html", message="The assignment was added successfully")
 
 
 @app.route("/edit_assignment/<int:edit_ID>", methods=["GET", "POST", "PUT"])
@@ -388,19 +408,25 @@ def edit_assignment(edit_ID=None):
     for i in request.form:
         if request.form[i] is None or request.form[i] == "":
             return render_template("edit_assignment.html", assignment_display=assignment_return,
-                                   message="The form must be filled out!")
+                                   message="The form must be filled out")
+
+    # Check if form entries are within 45 characters
+    for i in request.form:
+        if len(request.form[i]) > 45:
+            return render_template("edit_assignment.html", assignment_display=assignment_return,
+                                   message="Please enter 45 characters or fewer for each field")
 
     # Check if the assignment exists
     ed_assignment = Assignment.query.filter_by(assignment_ID=edit_ID).first()
     if ed_assignment is None:
         return render_template("edit_assignment.html", assignment_display=assignment_return,
-                               message="Nothing to edit!")
+                               message="Nothing to edit")
 
     # Check if the new assignment name is a duplicate
     assignment_check = Assignment.query.filter_by(assignment_name=request.form["assignment_name"]).first()
     if assignment_check is not None:
         return render_template("edit_assignment.html", assignment_display=assignment_return,
-                               message="There is already an assignment with this name!")
+                               message="There is already an assignment with this name")
 
     ed_assignment.assignment_name = request.form["assignment_name"]
     db.session.add(ed_assignment)
@@ -427,12 +453,12 @@ def delete_assignment(delete_ID=None):
     del_assignment = Assignment.query.filter_by(assignment_ID=delete_ID).first()
 
     if del_assignment is None:
-        return render_template("delete_assignment.html", message="Nothing to delete!")
+        return render_template("delete_assignment.html", message="Nothing to delete")
 
     db.session.delete(del_assignment)
     db.session.commit()
 
-    return render_template("delete_assignment.html", message="The assignment was deleted successfully!")
+    return render_template("delete_assignment.html", message="The assignment was deleted successfully")
 
 
 @app.route("/create_grade/<assign_get_ID>/<student_get_ID>/", methods=["GET", "POST"])
@@ -462,26 +488,26 @@ def create_grade(assign_get_ID=None, student_get_ID=None):
     for i in request.form:
         if request.form[i] is None or request.form[i] == "":
             return render_template("create_grade.html", assignment_display=assign_return,
-                                   student_display=student_return, message="The form must be filled out!")
+                                   student_display=student_return, message="The form must be filled out")
 
     # Check if the grade entry is a number:
     try:
         float(request.form["grade"])
     except ValueError:
         return render_template("create_grade.html", assignment_display=assign_return,
-                               student_display=student_return, message="You must enter a number!")
+                               student_display=student_return, message="You must enter a number")
 
     # Make sure there is no whitespace
     if " " in request.form["grade"]:
         return render_template("create_grade.html", assignment_display=assign_return,
-                               student_display=student_return, message="No whitespace can be entered!")
+                               student_display=student_return, message="No whitespace can be entered")
 
     # Check if there is already a matching grade entry
     match_check = Grade.query.filter_by(assignment_ID=assign_get_ID,
                                         student_ID=student_get_ID).first()
     if match_check is not None:
         return render_template("create_grade.html", assignment_display=assign_return,
-                               student_display=student_return, message="There is already a grade entry!")
+                               student_display=student_return, message="There is already a grade entry")
 
     new_grade = Grade()
     new_grade.grade = request.form["grade"]
@@ -491,7 +517,7 @@ def create_grade(assign_get_ID=None, student_get_ID=None):
     db.session.commit()
 
     return render_template("create_grade.html", assignment_display=assign_return,
-                           student_display=student_return, message="The grade was added successfully!")
+                           student_display=student_return, message="The grade was added successfully")
 
 
 @app.route("/edit_grade/<assign_get_ID>/<student_get_ID>/", methods=["GET", "POST"])
@@ -509,10 +535,10 @@ def edit_grade(assign_get_ID=None, student_get_ID=None):
                                          Grade.student_ID == Student.student_ID,
                                          Grade.assignment_ID == Assignment.assignment_ID).first()
     else:
-        return render_template("edit_grade.html", grade_display="", message="No IDs!")
+        return render_template("edit_grade.html", grade_display="", message="No IDs")
 
     if request.method == "GET":
-            return render_template("edit_grade.html", grade_display=grade_return)
+        return render_template("edit_grade.html", grade_display=grade_return)
 
     # If post...
     # Validate input
@@ -520,22 +546,22 @@ def edit_grade(assign_get_ID=None, student_get_ID=None):
     for i in request.form:
         if request.form[i] is None or request.form[i] == "":
             return render_template("edit_grade.html", grade_display=grade_return,
-                                   message="The form must be filled out!")
+                                   message="The form must be filled out")
 
     # Check if the grade entry is a number:
     try:
         float(request.form["grade"])
     except ValueError:
-        return render_template("edit_grade.html", grade_display=grade_return, message="You must enter a number!")
+        return render_template("edit_grade.html", grade_display=grade_return, message="You must enter a number")
 
     # Make sure there is no whitespace
     if " " in request.form["grade"]:
-        return render_template("edit_grade.html", grade_display=grade_return, message="No whitespace can be included!")
+        return render_template("edit_grade.html", grade_display=grade_return, message="No whitespace can be included")
 
     # Get the grade entry, make sure it exists
     ed_grade = Grade.query.filter_by(assignment_ID=assign_get_ID, student_ID=student_get_ID).first()
     if ed_grade is None:
-        return render_template("edit_grade.html", message="The grade could not be edited!")
+        return render_template("edit_grade.html", message="The grade entry could not be found")
 
     ed_grade.grade = request.form["grade"]
     db.session.add(ed_grade)
@@ -563,7 +589,7 @@ def delete_grade(assign_get_ID=None, student_get_ID=None, message=None):
             return render_template("delete_grade.html", grade_display=grade_return)
 
         else:
-            return render_template("delete_grade.html", grade_display="", message="No IDs!")
+            return render_template("delete_grade.html", grade_display="", message="No IDs")
 
     # If post...
 
@@ -573,12 +599,12 @@ def delete_grade(assign_get_ID=None, student_get_ID=None, message=None):
             "delete_grade.html",
             assignment_ID=assign_get_ID,
             student_ID=student_get_ID,
-            message="The entry could not be found to be deleted!"
+            message="The entry could not be found to be deleted"
         )
     db.session.delete(del_grade)
     db.session.commit()
 
-    return render_template("delete_grade.html", grade_display=del_grade, message="The grade has been deleted!")
+    return render_template("delete_grade.html", grade_display=del_grade, message="The grade has been deleted")
 
 
 # Models
